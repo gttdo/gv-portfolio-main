011142d87033ff70305e65d1c06d4b2175bc3263
gttdo <vinces.gerardo@gmail.com>
2021-09-09 10:39:27 -0700

    remove disable property from button

--- src/components/core-ui/Button/FixedButtonLink.js
+++ src/components/core-ui/Button/FixedButtonLink.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const FixedButtonLink = ({ addClass, linkTo, newPage, rel, textContent, variant, disabled }) => {
   return (
     <Link to={linkTo}>
-      <button disabled={`disabled ? true : false`} className={`FixedButton ${variant} ${addClass}`}>{textContent}</button>
+      {/* <button disabled={`disabled ? true : false`} className={`FixedButton ${variant} ${addClass}`}>{textContent}</button> */}
+      <button className={`FixedButton ${variant} ${addClass}`}>{textContent}</button>
     </Link>
 
   )
